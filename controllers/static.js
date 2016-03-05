877f91658485661b9284bceca62045ad9af0b2f9
chooco <choo_co@naver.com>
2016-03-05 10:44:31 +0900

    modularized

---
+++ controllers/static.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var router = require('express').Router()
+var path = require('path')
+
+router.use(express.static(path.resolve('assets')))
+
+router.get('/', function(req, res) {
+  res.sendFile(path.resolve('layouts/posts.html'))
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,36 +1,11 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var Post = require('./model/post')
 
 var app = express()
 app.use(bodyParser.json())
 
-app.get('/', function(req, res) {
-  res.sendFile('layouts/posts.html', { root: __dirname })
-})
-
-app.get('/api/posts', function(req, res, next) {
-  Post.find(function(err, posts) {
-    if (err) {
-      return next(err)
-    }
-    res.json(posts)
-  })
-})
-
-app.post('/api/posts', function(req, res, next) {
-  var post = new Post({
-    username: req.body.username,
-    body: req.body.body
-  })
-
-  post.save(function(err, post) {
-    if (err) {
-      return next(err)
-    }
-    res.status(201).json(post)
-  })
-})
+app.use('/', require('./controllers/static'))
+app.use('/api/posts', require('./controllers/api/posts'))
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
